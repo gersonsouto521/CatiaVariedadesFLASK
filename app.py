0c08a8159f8cc0b2a0e658a2f474aa04cb6d3fd5
gersonsouto521 <46946962+gersonsouto521@users.noreply.github.com>
2020-09-15 00:24:22 -0300

    ajustes para dados sem head, limite name, pequena refatoracao em variaveis

--- app.py
+++ app.py
@@ -12,21 +12,39 @@ def home():
     return render_template('index.html')
 
 @app.route('/relatorio', methods=['POST'])
-def relatorio():
-    lista = request.form['relatorio']
-    tuca = StringIO(str(lista))
-    df = pd.read_csv(tuca, sep="	")
-    tamanho = len(df)
-    return render_template('relatorio.html',df=df,tamanho=tamanho)
+def imprimeRomaneio(): 
+        planilhaWishLocal = request.form['relatorio']
+        if planilhaWishLocal[0:5] == 'Order':
+            dados = StringIO(str(planilhaWishLocal))
+            dataFrame = pd.read_csv(dados, sep="	")
+            tamanhoDados = len(dataFrame)
+            return render_template('relatorio.html',df=dataFrame,tamanho=tamanhoDados)
+        else:
+            addCabecalho = ('Order Sent Date	Order ID	Customer Name	Address	Phone	Product Name	SKU	Quantity	Package ID	Package Identifer\n'+planilhaWishLocal)
+            dados = StringIO(str(addCabecalho))
+            dataFrame = pd.read_csv(dados, sep="	")
+            tamanhoDados = len(dataFrame)
+            return render_template('relatorio.html',df=dataFrame,tamanho=tamanhoDados)
+
 
 @app.route('/etiquetas', methods=['POST'])
-def etiquetas():
-    info_etiquetas = request.form['etiquetas']
-    tuca = StringIO(str(info_etiquetas))
-    df = pd.read_csv(tuca, sep="	")
-    tamanho = len(df)
-    return render_template('etiquetas.html',df=df,tamanho=tamanho)
-    
+def imprimeEtiqueta():
+        planilhaWishLocal = request.form['etiquetas']
+        if planilhaWishLocal[0:5] == 'Order':
+            dados = StringIO(str(planilhaWishLocal))
+            dataFrame = pd.read_csv(dados, sep="	")
+            tamanhoDados = len(dataFrame)
+            return render_template('etiquetas.html',df=dataFrame,tamanho=tamanhoDados)
+        else:
+            addCabecalho = ('Order Sent Date	Order ID	Customer Name	Address	Phone	Product Name	SKU	Quantity	Package ID	Package Identifer\n'+planilhaWishLocal)
+            dados = StringIO(str(addCabecalho))
+            dataFrame = pd.read_csv(dados, sep="	")
+            tamanhoDados = len(dataFrame)
+            return render_template('etiquetas.html',df=dataFrame,tamanho=tamanhoDados)
+
+
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
